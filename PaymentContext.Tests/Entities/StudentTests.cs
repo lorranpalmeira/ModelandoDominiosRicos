df0fa99628ecab03ad762cc68de6ecac4eab4f4e
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-07 19:24:00 -0300

    Refact Testes

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -9,6 +9,24 @@ namespace PaymentContext.Tests{
     [TestClass]
     public class StudentTests{
 
+
+        private readonly Name _name;
+        private readonly Document _document;
+        private readonly Address _address;
+        private readonly Email _email;
+        private readonly Student _student;
+        private readonly Subscription _subscription;
+
+        public StudentTests()
+        {
+            _name =  new Name("Bruce","Wayne");
+            _document = new Document("123456789",EDocumentType.CPF);;
+            _address = new Address("Rua 1",1234,"Bairro Legal", "Gothan","SP","BR","12300-000");
+            _email = new Email("bruce@bruce.com");
+            _student = new Student(_name,_document,_email);
+            _subscription =  new Subscription(null);
+        }
+
         [TestMethod]
         public void AdcionarAssinatura(){
             // var subscription = new Subscription(null);
@@ -19,40 +37,37 @@ namespace PaymentContext.Tests{
 
         [TestMethod]
         public void ShouldReturnErrorWhenHadActiveSubscription(){
-            var name = new Name("Bruce","Wayne");
-            var document = new Document("123456789",EDocumentType.CPF);
-            var address = new Address("Rua 1",1234,"Bairro Legal", "Gothan","SP","BR","12300-000");
-            var email = new Email("bruce@bruce.com");
-            var student = new Student(name,document,email);
-            var subscription = new Subscription(null);
-            var payment = new PayPalPayment("12345678",DateTime.Now,DateTime.Now.AddDays(5),10,10,document,"Wayne",address,email);
+           
+            var payment = new PayPalPayment("12345678",DateTime.Now,DateTime.Now.AddDays(5),10,10,_document,"Wayne",_address,_email);
 
-            subscription.AddPayments(payment);
-            student.AddSubscription(subscription);
-            student.AddSubscription(subscription);
+            _subscription.AddPayments(payment);
+            _student.AddSubscription(_subscription);
+            _student.AddSubscription(_subscription);
 
-            Assert.Fail();
+            Assert.IsTrue(_student.Invalid);
 
         }
 
         [TestMethod]
-        public void ShouldReturnSucessWhenSubscriptionNoPayment(){
+        public void ShouldReturnErrorWhenSubscriptionHasNoPayment(){
 
-            var name = new Name("Bruce","Wayne");
-            var document = new Document("123456789",EDocumentType.CPF);
-            var address = new Address("Rua 1",1234,"Bairro Legal", "Gothan","SP","BR","12300-000");
-            var email = new Email("bruce@bruce.com");
-            var student = new Student(name,document,email);
+            _student.AddSubscription(_subscription);
 
-            Assert.Fail();
+            Assert.IsTrue(_student.Invalid);
             
         }
 
         [TestMethod]
         public void ShouldReturnSucessWhenHadActiveSubscription(){
+            var payment = new PayPalPayment("12345678",DateTime.Now,DateTime.Now.AddDays(5),10,10,_document,"Wayne",_address,_email);
+
+            _subscription.AddPayments(payment);
+            _student.AddSubscription(_subscription);
             
 
-            Assert.Fail();
+            Assert.IsTrue(_student.Valid);
+
+            
 
         }
 
