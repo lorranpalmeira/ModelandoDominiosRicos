a35e5774d0608e0b7490eec094ff5efa6ece49fe
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-08 19:02:01 -0300

    Repository Pattern

---
+++ PaymentContext.Domain/Repositories/IStudentRepository.cs
@@ -0,0 +1,11 @@
+namespace PaymentContext.Domain.Repositories
+{
+    public interface IStudentRepository
+    {
+        bool DocumentExists(string document);
+
+        bool EmailExists(string email);
+
+        void CreateSubscription(IStudentRepository student);
+    } 
+}
