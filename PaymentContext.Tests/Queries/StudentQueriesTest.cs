cea2c85055598747421f34b69ff9315d7ffadf01
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-12 17:49:14 -0300

    Queries Testes

--- PaymentContext.Tests/Queries/StudentQueriesTest.cs
+++ PaymentContext.Tests/Queries/StudentQueriesTest.cs
@@ -1,4 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Tests.Queries
 {
@@ -7,7 +13,29 @@ namespace PaymentContext.Tests.Queries
     public class StudentQueriesTest
     {
 
+        private IList<Student> _students;
+
+        public StudentQueriesTest()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                _students.Add(new Student(
+                    new Name("Aluno", i.ToString()),
+                    new Document("1111111111"+ i.ToString(),EDocumentType.CPF),
+                    new Email(i.ToString() + "@balta.io")
+                ));
+            }
+        }
         
+
+        [TestMethod]
+        public void ShouldReturnNullWhenDocumentNotExists(){
+
+            var exp = StudentQueries.GetStudentInfo("12345678911");
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreEqual(null,studn);
+        }
         
     }
 }
